2659595a2319f343516b31848ebc1ed483040390
Abdiitb <aryan223653badkul@gmail.com>
2025-05-17 01:50:36 +0530

    add blog and course models, controllers, and routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,9 +6,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-// Route import
+// Contact Router import
 import contactRouter from "./routes/contact.route.js";
-
+// Contact Routes
 app.use('/api/v1/contact', contactRouter);
+
+// Course Router import
+import courseRouter from "./routes/course.route.js";
+// Course Routes
+app.use('/api/v1/course', courseRouter);
+
+// Blog Router import
+import blogRouter from "./routes/blog.route.js";
+// Blog Routes
+app.use('/api/v1/blog', blogRouter);
 
 export { app };
